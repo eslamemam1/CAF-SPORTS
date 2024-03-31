631d26ea291fd53cdc5db34492b41d46e16c10e2
eslam emam <89427236+eslamemam1@users.noreply.github.com>
2024-03-31 17:00:28 +0200

    update slider

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -12,11 +12,11 @@ const ImageSlider = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 900,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
   };
 
   return (
