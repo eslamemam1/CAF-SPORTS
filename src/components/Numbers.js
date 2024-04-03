95a96825aed91f6cef0d8dd70c96c6ffa4378d33
eslam emam <89427236+eslamemam1@users.noreply.github.com>
2024-04-03 17:41:53 +0200

    update

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -5,7 +5,7 @@ const Numbers = () => {
     <div className=" flex flex-col sm:flex-row">
       <div className="w-full sm:w-[50%] h-[100px] bg-gray-800">
         <div className=" flex justify-center items-center h-[100px]">
-          <span className=" font-semibold text-4xl sm:text-5xl pr-2 text-white"><NumberCounter targetNumber={10} /> </span>
+          <span className=" font-semibold text-4xl sm:text-5xl pr-2 text-white"><NumberCounter targetNumber={13} /> </span>
           <span className=" text-[15px] md:text-[20px] font-normal text-start text-white leading-[20px]">
             Years of <br />
             Experienced
@@ -14,7 +14,7 @@ const Numbers = () => {
       </div>
       <div className="w-full sm:w-[50%] h-[100px] bg-[#00a44f]">
         <div className=" flex justify-center items-center h-[100px]">
-          <span className=" font-semibold text-4xl sm:text-5xl pr-2 text-white"><NumberCounter targetNumber={200} /></span>
+          <span className=" font-semibold text-4xl sm:text-5xl pr-2 text-white"><NumberCounter targetNumber={770} /></span>
           <p className=" text-[15px] md:text-[20px] font-normal text-start text-white leading-[20px]">
             Project
             <br />
